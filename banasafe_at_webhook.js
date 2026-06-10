8e93b1abb69d173cc3a4bd96ae389a57b7d14070
BanaSafe Project <atangnnono@gmail.com>
2026-06-11 00:48:19 +0200

    Create banasafe_at_webhook.js

---
+++ banasafe_at_webhook.js
@@ -0,0 +1,17 @@
+{
+  "name": "banasafe-ussd",
+  "version": "1.0.0",
+  "description": "BanaSafe USSD bridge",
+  "main": "banasafe_at_webhook.js",
+  "scripts": {
+    "start": "node banasafe_at_webhook.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.0",
+    "dotenv": "^16.3.1",
+    "express": "^4.18.2"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  }
+}
